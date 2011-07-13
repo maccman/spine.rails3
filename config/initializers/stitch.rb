cff34b6accf6148024a3c686fbc66ae6fc802eaa
Alex MacCaw <maccman@gmail.com>
2011-07-12 22:47:24 -0500

    update stitch

--- config/initializers/stitch.rb
+++ config/initializers/stitch.rb
@@ -3,15 +3,3 @@ class StitchApplication < Stitch::Server
     super(:paths => ["app/assets/javascripts/app", "app/assets/javascripts/lib"])
   end
 end
-
-module Stitch
-  class TmplCompiler < Stitch::Compiler
-    extensions :tmpl
-
-    def compile(path)
-      content = File.read(path)
-      %{var template = jQuery.template(#{content.to_json});
-        module.exports = (function(data){ return jQuery.tmpl(template, data); });}
-    end
-  end
-end
