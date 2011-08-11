c39aca0557e5a942c7bd0a1e247df9a2b90cc786
Alex MacCaw <maccman@gmail.com>
2011-08-11 10:05:43 +0100

    compile

--- config/initializers/stitch.rb
+++ config/initializers/stitch.rb
@@ -1,5 +1,9 @@
-class StitchApplication < Stitch::Server
+class StitchApplication
   def initialize
-    super(:paths => ["app/assets/javascripts/app", "app/assets/javascripts/lib"])
+    @package = Stitch::Package.new(:paths => ["app/assets/javascripts/app", "app/assets/javascripts/lib"])
+  end
+  
+  def call(env)
+    [200, {"Content-Type" => "text/javascript"}, [Uglifier.compile(@package.compile)]]
   end
 end
