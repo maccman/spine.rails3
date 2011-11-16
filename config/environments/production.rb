93188cddc742876828092be5c57d7173affacdaa
Alex MacCaw <maccman@gmail.com>
2011-11-15 17:31:06 -0800

    make work on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Testdefaults::Application.configure do
+SpineRails3::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
